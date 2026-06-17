b33eb24943bbf0eda18b243c6fb0ece7994a41c8
snehildwivedi03 <snehildwivedi03@gmail.com>
2026-06-17 21:04:19 +0530

    vite file changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,9 +12,6 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-<<<<<<< Updated upstream
-=======
     allowedHosts: ["plots-knight-lady-duration.trycloudflare.com"],
->>>>>>> Stashed changes
   },
-})
+});
